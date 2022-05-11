8aa29cea66d4874f66566e662e4ae9d48c614c82
SDranim01~ <ranimsaoudi90@gmail.com>
2022-05-11 14:56:01 +0100

    UserListAPI

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import UsersList from './Components/Userlist';
+import './CSS/Card.css'
+import './CSS/Details.css'
+import './CSS/Load.css'
+import Details from './Components/Details';
+import { Route, Routes } from 'react-router';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className='TitleApp'>List of users</h1>
+      
+       <Routes>
+       <Route path="/" element={<UsersList />} />
+       <Route path="/details/:id" element={<Details />} />
+       </Routes>
     </div>
   );
 }
